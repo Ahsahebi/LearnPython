b23350be880be90cecd62a95837055977f744156
Amirhosein sahebi <148428927+Ahsahebi@users.noreply.github.com>
2023-11-02 20:20:49 +0330

    Create Age.py

---
+++ Age.py
@@ -0,0 +1,13 @@
+Age = int(input())
+if Age >= 0 and Age < 6 :
+    print ("khordsal")
+elif Age >= 6 and Age < 10 :
+    print ("koodak")
+elif Age >= 10 and Age < 14 :
+    print ("nojavan")
+elif Age >= 14 and Age < 24 :
+    print ("javan")
+elif Age >= 24 and Age < 40 :
+    print ("bozorgsal")
+else:
+     print ("miansal")
