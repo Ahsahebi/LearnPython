f6daa45137e79275728cfebc7536fe9a909f0d1d
Ahsahebi <148428927+Ahsahebi@users.noreply.github.com>
2023-11-08 18:15:25 +0330

    Update Salary.py

--- Salary.py
+++ Salary.py
@@ -2,8 +2,8 @@ Per_Hour = int(input("How much do you get paid per hour?"))
 Hours = int (input("How many hours did you work?"))
 def Salary (Per_Hour, Hours):
     if Hours > 220 :
-       return "Saat kar bish az 220 nemitavanad bashad")
+       return ("Saat kar bish az 220 nemitavanad bashad")
     else :
         Total_incom = Hours * Per_Hour
         return Total_incom
-print ( Salary (Per_Hour, Hours))
+print (Salary (Per_Hour, Hours))
