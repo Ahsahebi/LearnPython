af1075106f7bb51f3894a70e0b3a9b0dc1040e39
Amirhosein sahebi <148428927+Ahsahebi@users.noreply.github.com>
2023-11-09 12:17:42 +0330

    Update Game2.py

--- Game2.py
+++ Game2.py
@@ -2,11 +2,11 @@ import random
 Happened = random.randint(1,100)
 print(Happened)
 Answer = input()
-while Answer != "Doroste":
-    if Answer == "Koochektare":
+while Answer != "d":
+    if Answer == "k":
         Happened = random.randint(1,Happened)
         print(Happened)
-    elif Answer == "bozorgtare":
+    elif Answer == "b":
         Happened = random.randint(Happened,100)
         print(Happened)
     Answer = input()
