f601bd30c09d1f9427cafb878f5fb8382b64a1d7
Amirhosein sahebi <148428927+Ahsahebi@users.noreply.github.com>
2023-11-10 07:02:39 +0330

    Update divisor.py

--- divisor.py
+++ divisor.py
@@ -1,16 +1,14 @@
 def divisor(a):
     b = 0
-    for i in range (1,1+a):
-        if a % i == 0:
+    for i in range(1, 1+a):
+        if a % i == 0 :
             b += 1
-    return b 
-Max_Count = 0
-for j in range(5):
+        return b
+Count_max= 0
+for j in range (20):
     Num = int(input())
     Count_divisor = divisor(Num)
-    if Count_divisor > Max_Count:
-        Max_Count = Count_divisor
-print(Num , Max_Count)
-
-        
+    if Count_divisor > Count_max:
+        Count_max = Count_divisor
+print (Num , Count_max)
     
