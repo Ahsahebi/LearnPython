0be22e97483522e3df1e2148e26ae6d1dc625475
Ahsahebi <148428927+Ahsahebi@users.noreply.github.com>
2023-11-15 13:21:48 +0330

    Update Game_count.py

--- Game_count.py
+++ Game_count.py
@@ -1,10 +1,11 @@
 Count = int(input())
+A = 0
 Player =[]
 for i in range(Count):
     Player_count = int(input())
     Player.append(Player_count)
 Player.sort()
 for j in range(0,Count):
-    if (Player[j] > 2):
-        Player.remove(Player[j])
-print(len(player)//3)
+    if (Player[j] <= 2):
+        A += 1
+print(A//3)
