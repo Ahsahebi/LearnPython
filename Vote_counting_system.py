1e73a14011a1d6e6f4005e81589cb0e587b3d742
Ahsahebi <148428927+Ahsahebi@users.noreply.github.com>
2023-11-16 18:04:43 +0330

    Update Vote_counting_system.py

--- Vote_counting_system.py
+++ Vote_counting_system.py
@@ -1,8 +1,15 @@
 Number = int(input())
-Name_total = ""
+Name_total =[]
+vote_counting =dict()
 for i in range(Number):
     Name = input()
-    Name_total += Name + " "
-for word in Name_total:
-    Name_total.find(word)
-print(Name_total)
+    Name_total.append(Name)
+for j in Name_total:
+    if j in vote_counting.keys():
+        vote_counting[j] += 1
+    else:
+        vote_counting[j] = 1
+Name_list = list(vote_counting.keys())
+Name_list.sort()
+for k in Name_list:
+    print(k ,vote_counting[k])
